a5c542995ca1f2bdf5a1c06c634ba92eab519e75
user <matthewhoover2013@gmail.com>
2019-02-16 21:29:10 -0800

    [dbHandler] Added get endpoint

--- server/db/db_test.go
+++ server/db/db_test.go
@@ -5,18 +5,26 @@ import (
 	"testing"
 )
 
-func TestDeleteWishList(t *testing.T) {
+func setupDb() (*DB, error) {
 	db, _ := InitDB(":memory:")
 	f, err := ioutil.ReadFile("../../sql/wishlist.sql")
 	if err != nil {
-		t.Error(err)
+		return nil, err
 	}
 	_, err = db.db.Exec(string(f))
 	if err != nil {
-		t.Error(err)
+		return nil, err
 	}
 
-	db.db.Exec(`INSERT INTO wishlist (id, userId) VALUES (1, 1)`)
+	_, err = db.db.Exec(`INSERT INTO wishlist (id, userId) VALUES (1, 1)`)
+	return db, err
+}
+
+func TestDeleteWishList(t *testing.T) {
+	db, err := setupDb()
+	if err != nil {
+		t.Error(err)
+	}
 
 	cases := []struct {
 		name     string
@@ -40,6 +48,42 @@ func TestDeleteWishList(t *testing.T) {
 			id:   1,
 			expected: func(e error) bool {
 				return e != nil
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			err := db.DeleteWishList(c.id)
+			if !c.expected(err) {
+				t.Error(err)
+			}
+		})
+	}
+}
+
+func TestGetWishList(t *testing.T) {
+	db, err := setupDb()
+	if err != nil {
+		t.Error(err)
+	}
+
+	cases := []struct {
+		name     string
+		id       int
+		expected func(error) bool
+	}{
+		{
+			name: "Get for missing id fails",
+			id:   2,
+			expected: func(e error) bool {
+				return e != nil
+			},
+		}, {
+			name: "Get for existing id returns it",
+			id:   1,
+			expected: func(e error) bool {
+				return e == nil
 			},
 		},
 	}
--- server/handlers/wishlist.go
+++ server/handlers/wishlist.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/Hoovs/OpenLibraryClient/server/db"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -14,8 +15,31 @@ type WishListHandler struct {
 	Db     *db.DB
 }
 
+func (wh *WishListHandler) PostWishListHandler(w http.ResponseWriter, r *http.Request) {
+	wh.Logger.Info("WishListHandler for post called")
+}
+
 func (wh *WishListHandler) GetWishListHandler(w http.ResponseWriter, r *http.Request) {
 	wh.Logger.Info("WishListHandler for get called")
+	vars := mux.Vars(r)
+	idStr := vars["wishListId"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to parse wish list id from request"))
+		return
+	}
+
+	row, err := wh.Db.GetWishList(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to fetch row"))
+		return
+	}
+	b, err := json.Marshal(row)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+	return
 }
 
 func (wh *WishListHandler) DeleteWishListHandler(w http.ResponseWriter, r *http.Request) {
